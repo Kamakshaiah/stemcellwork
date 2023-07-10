a1cadb47f79d4632a87137dcb200aaa4825944ab
Kamakshaiah <dr.m.kamakshaiah@gmail.com>
2023-07-10 15:10:33 +0530

    updated sem

--- R/scripts/stem-cell.R
+++ R/scripts/stem-cell.R
@@ -52,6 +52,9 @@ inspect(removeSparseTerms(abstracts_dtm, 0.8))
 sc_dense_data <- inspect(removeSparseTerms(abstracts_dtm, 0.99))
 sc_dense_df <- as.data.frame(sc_dense_data)
 
+# correlation
+cor(sc_dense_df)
+
 # names(sc_dense_df)
 # typeof(sc_dense_df)
 
@@ -102,3 +105,87 @@ plot(fit) # plot results
 summary(fit) # display the best model
 
 # https://www.geeksforgeeks.org/k-means-clustering-in-r-programming/
+
+# cfa
+
+sc_fa_df <- scale(sc_dense_df)
+fa_out <- fa(cov(sc_fa_df), 3)
+fa_out
+summary(fa_out)
+
+library(lavaan)
+
+
+sc_model  <- '
+
+  # latent 
+  stemcell =~ cell + cells + stem
+  medical =~ clinical + drug + treatment
+  service =~ marketing + patients
+  
+  # regressions
+  service ~ medical
+  service ~ stemcell
+  medical ~ stemcell
+'
+
+fit_sem <- sem(model = sc_model, data  = sc_dense_df)
+fit_cfa <- cfa(model = sc_model, data  = sc_dense_df)
+summary(fit_cfa)
+
+write.csv(data.frame(fit_sem_summary$pe), 'sem_output.csv')
+
+sc_med_model  <- '
+
+  # latent 
+  stemcell =~ cell + cells + stem
+  medical =~ clinical + drug + treatment
+  service =~ marketing + patients
+  
+  # direct
+  service ~ c*stemcell
+  
+  # mediation
+  
+  medical ~ a*stemcell
+  service ~ b*medical 
+'
+
+fit_sem_med <- sem(model = sc_med_model, data  = sc_fa_df)
+# fit_cfa <- cfa(model = sc_model, data  = sc_dense_df)
+summary(fit_sem_med)
+
+sc_med_model  <- '
+
+  # latent 
+  stemcell =~ cell + cells + stem
+  medical =~ clinical + drug + treatment
+  service =~ marketing + patients
+  
+  # clinical_decisions =~ medical + stemcell
+  # 
+  # # regressions
+  # clinical_decisions ~ stemcell
+  # clinical_decisions ~ medical
+  
+  # direct
+  stemcell ~ c*medical
+  
+  # mediation
+  
+  service ~ a*stemcell
+  medical ~ b*service 
+  
+  # indirect effect (a*b)
+  ab := a*b
+  
+  # total effect
+  total := c + (a*b)
+'
+fit_sem_med <- sem(model = sc_med_model, data  = sc_fa_df)
+summary(fit_sem_med)
+
+write.csv(data.frame(summary(fit_sem_med)$pe), 'sem_med_output.csv')
+
+library(semPlot)
+semPaths(fit_sem_med, )
